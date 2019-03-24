1a58ac8aefcf5ed4b1ad44b957b1eefb9aad58a3
Anton Sapunov <fort.sav.28@gmail.com>
2019-03-24 15:37:43 +0700

    Refactoring

--- main.c
+++ main.c
@@ -35,6 +35,7 @@ void fill_zero(int* matrix, size_t elements_amount)
 // output - указатель на начало матрицы
 // l_num - количество строк
 // с_num - количество столбцов
+// pid - момер вызывающего процесса
 void print_matrix(int* output, size_t l_num, size_t c_num, int pid)
 {
     for(size_t i = 0; i < l_num; ++i)
@@ -49,19 +50,47 @@ void print_matrix(int* output, size_t l_num, size_t c_num, int pid)
     printf("I`m process %d!\n\n", pid);
 }
 
+// Извлечение трёх средних столбцов из матрицы
+// matrix - указатель на начало исходной матрицы
+// l_num - количество строк в исходной матрице
+// с_num - количество столбцов в исходной матрице
+// three_columns - указатель на начало извлечённой матрицы
+// needed_columns - количество столбцов в извлечённой матрице
+void extract_three_columns(int* matrix, size_t l_num, size_t c_num, int* three_columns, size_t needed_columns)
+{
+    size_t j  = 0;
+    for(size_t line_counter = 1; line_counter < l_num * c_num; line_counter += 5)
+    {
+        for(size_t i = 0; i < needed_columns; ++i)
+        {
+            // Set value to three_columns element
+            *(three_columns + i + j) = *(matrix + i + line_counter);
+        }
+
+        // Switch to next line of three_columns
+        j += 3;
+    }
+}
+
 int main (int argc, char* argv[])
 {
-    // Work with MPI
+    // Initialize MPI
     int errorCode;
     if ((errorCode = MPI_Init(&argc, &argv)) != 0)
     {
         return errorCode;
     }
 
+    // Get MPI data
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    if(world_size != 2)
+    {
+        printf("This program should work with 2 proceses, but has %d\n", world_size);
+        return errorCode;
+    }
 
     // Matrix
     size_t l_num = 6;
@@ -70,12 +99,10 @@ int main (int argc, char* argv[])
 
     matrix = (int*)malloc(l_num * c_num * sizeof(int));
     fill_zero(matrix, l_num * c_num);
-    //print_matrix(matrix, l_num, c_num, world_rank);
 
     // Part of matrix
     size_t needed_columns = 3;
     int* three_columns;
-    
     // Create needed columns
     three_columns = (int*)malloc(l_num * needed_columns * sizeof(int));
     
@@ -84,42 +111,21 @@ int main (int argc, char* argv[])
     MPI_Type_vector(l_num, needed_columns, needed_columns, MPI_INT, &mpi_three_columns);
     MPI_Type_commit(&mpi_three_columns);
 
-    // Initialize matrix only in main process
-    if(world_rank == 0)
-    {
-        //TODO: Создать ограничение на минимальное количество процессов.
-        // Если процессов меньше необходимого - аварийно завершать программу.
-        // Если сильно больше - тоже как-то прервать.
+    // Status for communicating
+    MPI_Status status;
 
+    if(world_rank == 0) 
+    {
         // Create the matrix
         fill_with_random_values(matrix, l_num * c_num);
         
         printf("Original matrix:\n");
         print_matrix(matrix, l_num, c_num, world_rank);
 
-        size_t j  = 0;
-        for(size_t line_counter = 1; line_counter < l_num * c_num; line_counter += 5)
-        {
+        // Create the result matrix
+        fill_with_random_values(matrix, l_num * c_num);
+        extract_three_columns(matrix, l_num, c_num, three_columns, needed_columns);
 
-                for(size_t i = 0; i < needed_columns; ++i)
-                {
-                    // Set value to three_columns element
-                    *(three_columns + i + j) = *(matrix + i + line_counter);
-                }
-
-                // Switch to next line of three_columns
-                j += 3;
-        }
-
-    }
-
-    MPI_Status status;
-
-    printf("____________________________________________________\n");
-    printf("Start communication!\n");
-
-    if(world_rank == 0) 
-    {
         printf("Needed part of matrix:\n");
         print_matrix(three_columns, l_num, needed_columns, world_rank);
 
